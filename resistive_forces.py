608f8b1ac1a76d505b4fa1eac768f5a28fa29794
Henrik Brokke <brokkehenrik@gmail.com>
2024-02-23 09:32:51 +0100

    Fix plot of resistive forces for incline

--- resistive_forces.py
+++ resistive_forces.py
@@ -17,6 +17,29 @@ rho = 1.2
 A = 0.4
 eta = 1
 
+def forces_pot(s):
+    vec = []
+    for v in range(5, 45, 5):
+        vec.append(m*g*s*v/3.6)
+    return vec
+
+def forces_roll():
+    vec = []
+    for v in range(5, 45, 5):
+        vec.append(my*m*g*v/3.6)
+    return vec
+
+def forces_bearing():
+    vec = []
+    for v in range(5, 45, 5):
+        vec.append((b0*v + b1*v**2)/3.6)
+    return vec
+
+def forces_drag():
+    vec = []
+    for v in range(5, 45, 5):
+        vec.append((0.5*Cd*rho*A*(v/3.6)**3))
+    return vec
 
 def forces(v, s):
     v = v/3.6
@@ -39,26 +62,29 @@ forces_decline = get_forces_vec(-0.05)
 speeds = np.arange(5, 45, 5)
 force_labels = ['Potential', 'Rolling', 'Bearing', 'Drag']
 
+potential_forces_incline = np.array(forces_pot(0.05))
+potential_forces_flat = np.array(forces_pot(0))
+potential_forces_decline = np.array(forces_pot(-0.05))
+rolling_forces = np.array(forces_roll())
+bearing_forces = np.array(forces_bearing())
+drag_forces = np.array(forces_drag())
 
-fig, ax = plt.subplots(3, 1, figsize=(10, 12))
+print(potential_forces_incline+rolling_forces)
 
-bar_width = 1
-bar_padding = 0.2
-speeds_shift = np.arange(len(speeds)) - 3 * bar_width - bar_padding
+N = 8
+index = np.arange(N)
+width = 0.35
+#fig, ax = plt.subplots(3, 1, figsize=(10, 12), sharex=True, sharey=True)
+print(forces_pot(0.05))
+print(forces_roll())
+plt.bar(index, potential_forces_incline, width)
+plt.bar(index, drag_forces, width, bottom=potential_forces_incline)
+plt.bar(index, rolling_forces, width, bottom=potential_forces_incline+drag_forces)
+plt.bar(index, bearing_forces, width, bottom=potential_forces_incline+rolling_forces+drag_forces)
 
-for i, (forces, title) in enumerate(zip([forces_flat, forces_incline, forces_decline], ['Flat Road', 'Incline', 'Decline'])):
-    for j, force_label in enumerate(force_labels):
-        ax[i].bar(speeds + j * (bar_width + bar_padding), [force[j] for force in forces], width=bar_width, label=force_label)
-
-    ax[i].set_title(title)
-    ax[i].set_xlabel('Speed (km/h)')
-    ax[i].set_ylabel('Force (N)')
-    ax[i].set_xticks(speeds)
-    ax[i].legend()
-
-plt.tight_layout()
-
-# Adjust spacing between subplots
-plt.subplots_adjust(hspace=0.5)
-
+plt.xlabel("Velocity [km/h]")
+plt.ylabel("Power [W]")
+plt.title("Force components at different velocities and gradients")
+plt.xticks(index, [5, 10, 15, 20, 25, 30, 35, 40])
+plt.legend(['Potential', 'Drag', 'Rolling', 'Bearing'])
 plt.show()
