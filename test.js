9af3ef87c30c016d3e6337496553a2f02272808e
Henrik Brokke <brokkehenrik@gmail.com>
2024-03-05 15:35:08 +0100

    Add simple interpolator in JS

---
+++ test.js
@@ -0,0 +1,8 @@
+const numeric = require('numeric');
+
+const distance = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+const slope = [0, 0.01, 0.01, 0.02, 0.04, 0.06, 0.06, 0.04, 0.02, 0.0, -0.01]
+
+const spline = numeric.spline(distance, slope)
+const interpolated_spline = spline.at(55)
+console.log(interpolated_spline)
