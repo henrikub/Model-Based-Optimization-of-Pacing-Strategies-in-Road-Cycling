ae665b8b92e9a25eec73da86e3913885f6c53c80
Henrik Brokke <brokkehenrik@gmail.com>
2024-03-19 10:06:09 +0100

    Add functionality to read json in JS

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
-const numeric = require('numeric');
+// const numeric = require('numeric');
 
-const distance = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-const slope = [0, 0.01, 0.01, 0.02, 0.04, 0.06, 0.06, 0.04, 0.02, 0.0, -0.01]
+// const distance = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+// const slope = [0, 0.01, 0.01, 0.02, 0.04, 0.06, 0.06, 0.04, 0.02, 0.0, -0.01]
 
-const spline = numeric.spline(distance, slope)
-const interpolated_spline = spline.at(55)
-console.log(interpolated_spline)
+// const spline = numeric.spline(distance, slope)
+// const interpolated_spline = spline.at(55)
+// console.log(interpolated_spline)
+console.log('hello')
+import data from './optimal_power.json' assert {type: 'json'}
+
+console.log(data)
