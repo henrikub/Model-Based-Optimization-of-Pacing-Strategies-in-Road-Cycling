1b49ffda221b1361193020cb178798423d93b863
Henrik Brokke <brokkehenrik@gmail.com>
2024-05-28 12:48:23 +0200

    change long filenames

--- data_anders.py
+++ data_anders.py
@@ -5,21 +5,14 @@ import datetime
 from scipy.ndimage import gaussian_filter1d
 from utils.utils import normalized_power
 
-# file_location = "Activities/2024-04-30 Zwift_-_Race__DIRT_Racing_Series_-_Rionda_-_Gemstones_-_Stage_6_(B)____2_laps_iTT_on_Two_Bridges_Loop_in_Watopia.tcx"
-# tcx_reader = TCXReader()
-# data = tcx_reader.read(file_location).trackpoints[0]
-# print(data)
-
-
-
-unpaced_tt = ActivityReader("2024-04-30 Zwift_-_Race__DIRT_Racing_Series_-_Rionda_-_Gemstones_-_Stage_6_(B)____2_laps_iTT_on_Two_Bridges_Loop_in_Watopia.tcx", garmin=False)
+unpaced_tt = ActivityReader("Anders_paced_tt.tcx", garmin=False)
 unpaced_tt.remove_period_before(170)
 unpaced_tt.remove_period_after(14410)
 unpaced_tt.distance = np.array(unpaced_tt.distance) - unpaced_tt.distance[0]
 unpaced_tt.time = np.array(unpaced_tt.time) - unpaced_tt.time[0]
 unpaced_power = gaussian_filter1d(unpaced_tt.power, 4)
 
-paced_tt = ActivityReader("2024-05-16 Zwift_Two_Bridges_Loop_iTT_pacing_mod_test_in_Watopia.tcx", garmin=False)
+paced_tt = ActivityReader("Anders_unpaced_tt.tcx", garmin=False)
 paced_tt.remove_period_before(7170)
 paced_tt.remove_period_after(21130)
 paced_tt.distance = np.array(paced_tt.distance) - paced_tt.distance[0]
